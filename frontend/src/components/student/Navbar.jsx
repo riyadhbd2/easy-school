92ffe0157128cb7b4e121096a4296796e7975b8d
riyadhbd2 <riyadhbd2@gmail.com>
2025-02-09 15:41:49 +0100

    navbar added

--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
-  return (
-    <div>
-        <h1>Navbar</h1>
-    </div>
-  )
-}
+  const isCourseListPage = location.pathname.includes("/course-list");
 
-export default Navbar
+  return (
+    <div
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
+        isCourseListPage ? "bg-white" : "bg-cyan-100/70"
+      }`}
+    >
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-28 lg:w-32 cursor-pointer"
+      />
+      <div className="hidden md:flex items-center gap-5 text-gray-500">
+        <div className="flex items-center gap-5">
+          <button>Become Educator </button> |{" "}
+          <Link to="/my-enrollemnts">My Enrollments</Link>
+        </div>
+        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
+          Create Account
+        </button>
+      </div>
+      {/* for small screen */}
+      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+        <div>
+          <button>Become Educator</button> |{" "}
+          <Link to="/my-enrollemnts">My Enrollments</Link>
+        </div>
+        <button>
+          <img src={assets.user_icon} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
